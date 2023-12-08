889ba8fa3ccd73541d69f504924f94dd04294859
eastalan <Aman Gupta>
2023-12-08 22:27:35 +0530

    Incoming data error handling-2

--- app.py
+++ app.py
@@ -18,9 +18,21 @@ stores=[{
 def get_stores():
     return{"Stores":list(stores.values())}
 
+
 @app.post("/store")
 def create_store():
     store_data=request.get_json()
+
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
     store_id =  uuid.uuid4().hex
     new_store={**store_data,"id":store_id}
     stores[store_id]=new_store
@@ -40,16 +52,33 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        abort(404,message = "Store not found")
+
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+
     item_id=uuid.uuid4().hex
     new_item = {**item_data,"id":item_id}
     items[item_id]=new_item
     return new_item,201
 
+
 @app.get("/store")
 def get_all_items():
     return{"items":list(items.values())}
+
 
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
@@ -57,6 +86,7 @@ def get_store(store_id):
         return stores[store_id]
     except KeyError:
         abort(404,message = "Store not found")
+
 
 @app.get("/item/<string:item_id>")
 def get_items(item_id):
