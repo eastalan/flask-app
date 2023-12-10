85f81f0a519edfa76c7f9aefc4869d6c9834aad2
eastalan <Aman Gupta>
2023-12-10 11:08:59 +0530

    ORM implementation with SQLlite

--- resources/items.py
+++ resources/items.py
@@ -3,8 +3,10 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from schemas import ItemSchema,ItemUpdateSchema
+from models import ItemModel
+from sqlalchemy.exc import SQLAlchemyError
 
-from db import items
+from db import db
 
 blp = Blueprint("Items", __name__, description="Operations on items")
 
@@ -13,53 +15,52 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 class Item(MethodView):
     @blp.response(200,ItemSchema)
     def get(self,item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404,message = "item not found")
-
+        item = ItemModel.query.get_or_404(item_id)
+        return item
+    
+    
     @blp.arguments(ItemUpdateSchema)
-    @blp.response(200,ItemSchema)
-    def put(self,item_data,item_id):
-        #no longer need item data to process request as its already being done
-        #item_data=request.get_json()
+    @blp.response(200, ItemSchema)
+    def put(self, item_data, item_id):
+        item = ItemModel.query.get(item_id)
 
-        try:
-            item=items[item_id]
-            item |= item_data
-            
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
 
-    def delete(self,item_id):
-        try:
-            del items[item_id]
-            return {"message":"item deleted"}
-        except KeyError:
-            abort(404,message = "item not found")
+        db.session.add(item)
+        db.session.commit()
+
+        return item
+    
+
+    def delete(self, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted."}
 
 
 @blp.route("/item")
 class ItemList(MethodView):
     @blp.response(200,ItemSchema(many=True))
     def get(self):
-        return items.values()
+        return ItemModel.query.all()
+        #return items.values()
     
     @blp.arguments(ItemSchema)
     @blp.response(201,ItemSchema)
     def post(self,item_data):
         #no longer need item data to process request as its already being done
         #item_data = request.get_json()
+        item = ItemModel(**item_data)
 
-        for item in items.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500,message="An error occured while inserting item")
 
-        item_id=uuid.uuid4().hex
-        new_item = {**item_data,"id":item_id}
-        items[item_id]=new_item
-        return new_item
+        return item
